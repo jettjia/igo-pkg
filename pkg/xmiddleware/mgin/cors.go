11c8d233609fb4628e0aca346b0c9ffcd6c8a040
jettjia <jettjia@qq.com>
2026-03-17 15:05:13 +0800

    feat:update cors

--- pkg/xmiddleware/mgin/cors.go
+++ pkg/xmiddleware/mgin/cors.go
@@ -14,7 +14,7 @@ func Cors() gin.HandlerFunc {
 		if origin != "" {
 			c.Header("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, HEAD")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Lang, User-Exp, X-Tenant-ID")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Lang, X-Language ,User-Exp, X-Tenant-ID")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			c.Header("Access-Control-Allow-Credentials", "true")
 		}
